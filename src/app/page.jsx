cd371b9c89c4afdd49786d58db69237abdb98bde
thotsaporn Jaisue (TECH-TJ) <98930665+thotsaphon19@users.noreply.github.com>
2026-06-21 07:19:18 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,3 @@
 import dynamic from 'next/dynamic';
-const App = dynamic(() => import('@/components/App'), { ssr: false });
+const App = dynamic(() => import('@/components/App'), { ssr: false, loading: () => null });
 export default function Page() { return <App />; }
